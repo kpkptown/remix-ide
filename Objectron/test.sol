58afc94cb050b8c9e96aafb0c5fe5d97ece4c268
kpkptown <ask.ksaay.2510@gmail.com>
2023-02-09 20:45:55 +0900

    :construction: Work in progress.

--- Objectron/test.sol
+++ Objectron/test.sol
@@ -1,79 +1,71 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
-import {ERC2981} from "@openzeppelin/contracts/token/common/ERC2981.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import {DefaultOperatorFilterer} from "./DefaultOperatorFilterer.sol";
 import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155URIStorage.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import {DefaultOperatorFilterer} from "./DefaultOperatorFilterer.sol";
 
-/**
- * @title  ExampleERC1155
- * @notice This example contract is configured to use the DefaultOperatorFilterer, which automatically registers the
- *         token and subscribes it to OpenSea's curated filters.
- *         Adding the onlyAllowedOperator modifier to the safeTransferFrom methods ensures that
- *         the msg.sender (operator) is allowed by the OperatorFilterRegistry. Adding the onlyAllowedOperatorApproval
- *         modifier to the setApprovalForAll method ensures that owners do not approve operators that are not allowed.
- */
-contract ExampleERC1155 is ERC1155URIStorage, Ownable, ERC2981, DefaultOperatorFilterer {
+contract ObjectronTest is DefaultOperatorFilterer, AccessControl, ERC1155URIStorage {
+
+    using Strings for string;
+
+    string public name = "0bjectr0n";
+    string public symbol = "0bjectr0n";
+
+    bytes32 public constant URI_SETTER_ROLE = keccak256("URI_SETTER_ROLE");
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
     constructor() ERC1155("") {
-        // _grantOperatorRole(msg.sender);
-        _setDefaultRoyalty(msg.sender, 1000);
-        _setBaseURI("ipfs://QmRoQtYEM6wkEXmL9PGf7qwWHR8Jrzc2oBN7snTgfFAjFB/");
-        initializeNFT(1, "1.json");
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(URI_SETTER_ROLE, msg.sender);
+        _grantRole(MINTER_ROLE, msg.sender);
+        // _setBaseURI("ipfs://QmRoQtYEM6wkEXmL9PGf7qwWHR8Jrzc2oBN7snTgfFAjFB/");
+        _setURI("ipfs://QmRoQtYEM6wkEXmL9PGf7qwWHR8Jrzc2oBN7snTgfFAjFB/2.json");
+        mint(msg.sender, 2, 2, "");
     }
 
-    function setBaseURI(string memory uri_) external onlyOwner {
-        _setBaseURI(uri_);
-    }
-
-    function initializeNFT(uint256 tokenId, string memory _tokenURI)
-        public
-        onlyOwner
-    {
-        require(bytes(uri(tokenId)).length == 0, "NFT already exists");
-        _mint(msg.sender, tokenId, 1, "");
-        _setURI(tokenId, _tokenURI);
-    }
-    /**
-     * @dev See {IERC1155-setApprovalForAll}.
-     *      In this example the added modifier ensures that the operator is allowed by the OperatorFilterRegistry.
-     */
     function setApprovalForAll(address operator, bool approved) public override onlyAllowedOperatorApproval(operator) {
         super.setApprovalForAll(operator, approved);
     }
 
-    /**
-     * @dev See {IERC1155-safeTransferFrom}.
-     *      In this example the added modifier ensures that the operator is allowed by the OperatorFilterRegistry.
-     */
-    function safeTransferFrom(address from, address to, uint256 tokenId, uint256 amount, bytes memory data)
+    // function uri(uint256 id) external view virtual override(ERC1155) returns (string memory) {
+    //     return super.uri(id);
+    // }
+
+    function setBaseURI(string memory uri_) external onlyRole(URI_SETTER_ROLE) {
+        _setBaseURI(uri_);
+    }
+
+    function setURI(string memory newuri) public onlyRole(URI_SETTER_ROLE) {
+        _setURI(newuri);
+    }
+
+    function mint(address account, uint256 id, uint256 amount, bytes memory data)
         public
-        override
-        onlyAllowedOperator(from)
+        onlyRole(MINTER_ROLE)
     {
-        super.safeTransferFrom(from, to, tokenId, amount, data);
+        _mint(account, id, amount, data);
     }
 
-    /**
-     * @dev See {IERC1155-safeBatchTransferFrom}.
-     *      In this example the added modifier ensures that the operator is allowed by the OperatorFilterRegistry.
-     */
-    function safeBatchTransferFrom(
-        address from,
-        address to,
-        uint256[] memory ids,
-        uint256[] memory amounts,
-        bytes memory data
-    ) public virtual override onlyAllowedOperator(from) {
-        super.safeBatchTransferFrom(from, to, ids, amounts, data);
+    function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
+        public
+        onlyRole(MINTER_ROLE)
+    {
+        _mintBatch(to, ids, amounts, data);
     }
 
-    /**
-     * @dev See {IERC165-supportsInterface}.
-     */
-    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC1155, ERC2981) returns (bool) {
-        return super.supportsInterface(interfaceId);
+    // The following functions are overrides required by Solidity.
+
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC1155, AccessControl)
+        returns (bool)
+    {
+        // if(interfaceId == 0xfa72c5f5) {}
+        return ERC1155.supportsInterface(interfaceId);
     }
 }
